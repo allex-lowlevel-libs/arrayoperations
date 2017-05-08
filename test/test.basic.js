81828d1a112ff31c3245b955aead73f498a74c72
Veljko Popovic <v.s.popovic@gmail.com>
2017-05-08 22:54:09 +0200

    introducing some deps and tests

--- test/test.basic.js
+++ test/test.basic.js
@@ -1,8 +1,8 @@
 var expect = require('chai').expect,
-  inherit = require('allex_inheritlowlevellib').inherit,
+  checkftions = require('allex_checkslowlevellib'),
+  inherit = require('allex_inheritlowlevellib')(checkftions.isFunction, checkftions.isString).inherit,
   AllexError = require('allex_errorlowlevellib')(inherit),
   AllexJSONizingError = require('allex_jsonizingerrorlowlevellib')(AllexError,inherit),
-  checkftions = require('allex_checkslowlevellib'),
   objmanip = require('allex_objectmanipulationlowlevellib')(checkftions),
   stringmanip = require('allex_stringmanipulationlowlevellib')(checkftions.isString, checkftions.isNull),
   dlinkedlistbase = require('allex_doublelinkedlistbaselowlevellib'),
@@ -55,6 +55,12 @@ describe('Basic tests', function () {
   });
   it('difference', function () {
     expect(arrOp.difference([1, 2, 3, '4', '5'], [1, '4', 8, 9, 10])).to.deep.equal([2, 3, '5']);
+    var r = arrOp.difference ([1,2,3], [4,5,6]);
+    if (arrOp.contains (r, 4)) throw new Error('Result should not contain 4');
+    if (arrOp.contains (r, 5)) throw new Error('Result should not contain 5');
+    if (arrOp.contains (r, 6)) throw new Error('Result should not contain 6');
+    r = arrOp.difference([1,2],[2,3]);
+    if (arrOp.contains(r, 2)) throw new Error('Result should not contain 2');
   });
   it('union', function () {
     expect(arrOp.union([1, '2', 2, 3, 'a'], [1, 2, 4, '4'])).to.deep.equal([1, '2', 2, 3, 'a', 4, '4']);
@@ -78,4 +84,69 @@ describe('Basic tests', function () {
     arrOp.appendNonExistingObjects(a1, a2, 'name');
     expect(a1).to.deep.equal([{name: 'a', width: 7}]);
   });
+});
+
+describe ('Test pivot', function () {
+  var numeric_source = [
+    {"symbol":0,"repetitions":5,"multiplier":1000},
+    {"symbol":0,"repetitions":4,"multiplier":500},
+    {"symbol":0,"repetitions":3,"multiplier":200},
+    {"symbol":2,"repetitions":5,"multiplier":300},
+    {"symbol":2,"repetitions":4,"multiplier":250},
+    {"symbol":3,"repetitions":5,"multiplier":250},
+    {"symbol":3,"repetitions":4,"multiplier":200},
+    {"symbol":4,"repetitions":5,"multiplier":220},
+    {"symbol":4,"repetitions":4,"multiplier":200},
+    {"symbol":4,"repetitions":3,"multiplier":160},
+    {"symbol":5,"repetitions":5,"multiplier":200},
+    {"symbol":5,"repetitions":4,"multiplier":180},
+    {"symbol":5,"repetitions":3,"multiplier":120},
+    {"symbol":6,"repetitions":5,"multiplier":180},
+    {"symbol":6,"repetitions":4,"multiplier":120},
+    {"symbol":6,"repetitions":3,"multiplier":75}];
+
+
+  var pivot = arrOp.pivot, 
+    unpivot = arrOp.unpivot,
+    Pivoter = arrOp.Pivoter;
+
+  it ('Pivot test', function () {
+    var r = pivot(numeric_source, {
+      x_field: 'repetitions', 
+      y_field: 'symbol', 
+      value_field: 'multiplier', 
+      init_empty_rows: false,
+      x_fields_list : ['2','3','4','5'],
+      to_y : function (s) { return parseInt(s); }
+    });
+    expect(r).to.be.an.array;
+    expect(r).to.have.length.of(7);
+    expect(r[1]).to.be.undefined;
+  });
+
+  it ('Pivot test', function () {
+    var p = new Pivoter({
+      x_field: 'repetitions', 
+      y_field: 'symbol', 
+      value_field: 'multiplier', 
+      init_empty_rows: false,
+      x_fields_list : [2,3,4,5],
+      init_empty_rows : true
+    }),
+      r = p.pivot(numeric_source);
+
+    expect(r).to.be.an.array;
+    expect(r).to.have.length.of(7);
+    expect(r[1]).to.deep.equal(p.initializeEmptyPivotRecord());
+
+    var r1 = p.unpivot (r),
+      diff = arrOp.difference(r1, numeric_source, function (i1, i2) {
+        return i1.symbol == i2.symbol && i1.multiplier == i2.multiplier && i1.repetitions == i2.repetitions;
+      });
+
+    expect(diff.filter(function (item) {
+      return !!item.multiplier;
+    })).to.be.empty;
+  });
+
 });
